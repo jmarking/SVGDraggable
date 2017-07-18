cb25bf7f1a2e07372e86f80bf0195b11b38a06ea
Jon Marking <jon.marking@hotmail.com>
2017-07-17 22:04:11 -0500

    Add polyfill for IE11.

---
+++ SVGDraggable/scripts/polyfills/element.closest.js
@@ -0,0 +1,13 @@
+﻿if (window.Element && !Element.prototype.closest) {
+    Element.prototype.closest =
+        function (s) {
+            var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+                i,
+                el = this;
+            do {
+                i = matches.length;
+                while (--i >= 0 && matches.item(i) !== el) { /*comment*/ }
+            } while (i < 0 && (el = el.parentElement));
+            return el;
+        };
+}
